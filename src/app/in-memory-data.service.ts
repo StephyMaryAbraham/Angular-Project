7075de08a4c896bd9ffcb2440fecf2be7c748548
Stephy Abraham <sabraham@protegra.com>
2019-07-22 12:45:04 -0500

    complete app

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Hero } from './hero';
 
 @Injectable({
   providedIn: 'root'
 })
-export class InMemoryDataService {
+export class InMemoryDataService implements InMemoryDbService{
 
   constructor() { }
   createDb() {
@@ -20,4 +22,13 @@ export class InMemoryDataService {
       { id: 20, name: 'Tornado' }
     ];
     return {heroes};
+  }
+  // Overrides the genId method to ensure that a hero always has an id.
+  // If the heroes array is empty,
+  // the method below returns the initial number (11).
+  // if the heroes array is not empty, the method below returns the highest
+  // hero id + 1.
+  genId(heroes: Hero[]): number {
+    return heroes.length > 0 ? Math.max(...heroes.map(hero => hero.id)) + 1 : 11;
+  }
 }
